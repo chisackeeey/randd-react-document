f017fe50abde485f020e400fbd793faf0d6b4728
chisackeeey <chisaki_h@outlook.jp>
2020-05-11 10:45:58 +0900

    ハンズオンリンクの追加

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "SPA入門",
   themeConfig: {
-    sidebar: ["/page1", "/page2"],
+    sidebar: ["/page1", "/page2", "/page3"],
   },
   markdown: {
     lineNumbers: true,
